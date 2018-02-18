b24a3461f14b65eab83889a2c2cf6289d63666fb
Joseph McBride <jmcbriddle@gmail.com>
2018-02-18 13:46:47 -0800

    add frontend folder

---
+++ frontend/components/map.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+class Map extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
